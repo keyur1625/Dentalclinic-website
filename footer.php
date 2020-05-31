8d2b3b05318e2e9510203dfb2f8cae18cbc5d542
keyur1625 <keyursomaiya88@gmail.com>
2020-05-31 17:49:21 +0530

    footer file included

---
+++ footer.php
@@ -0,0 +1,87 @@
+<footer class="footer-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-5 offset-1 col-sm-2">
+                    <h5>&nbsp;</h5>
+                    <br>
+                    <ul class="list-unstyled">
+                        <li><a href="index.php">Home</a></li>
+                          <li><a href="Services.php">Services</a></li>
+                       
+                        <li><a href="Opening-hour.php">Opening Hour</a></li>
+                        <li><a href="./Gallary.php">Gallary</a></li>
+                         <li><a href="./about.php">About</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-sm-5">
+                    <h5><font color="lightblue">Our Address</font></h5>
+                    <br>
+                    <address>
+		              Shri Krushman Co. Op. Housing Society,Block No.05<br>
+					  Babariya Main Road<br> 
+					  Rajkot-360002<br>
+		              <i class="fa fa-phone fa-lg"></i>:+91 6353624789<br>
+		              
+		              <!-- <i class="fa fa-envelope fa-lg"></i>:
+                      <a href="mailto:sudantdentalclinic1@gmail.com">sudantdentalclinic1@gmail.com</a> -->
+		           </address>
+                </div>
+    <div class="col-sm align-self-center">
+            <div style="text-align:center">
+                <a class="btn btn-social-icon btn-instagram" href="https://www.instagram.com/sudant_dental_clinic/" target="_black"><i class="fa fa-instagram"></i></a>
+                <a class="btn btn-social-icon btn-facebook" href="https://www.facebook.com/Sudant-Dental-Clinic-443039666275903/" target="_black"><i class="fa fa-facebook"></i></a>
+               
+                <a class="btn btn-social-icon" href="mailto:sudantdentalclinic1@gmail.com"  target="_blank"><i class="fa fa-envelope-o"></i></a>
+            </div>
+        </div>
+   </div>
+				<!-- 		<div class="col-lg-2  col-md-6">
+							<div class="single-footer-widget">
+								<h6>&nbsp;</h6>
+								<ul class="footer-nav">
+									<li><a href="#">Address</a></li>
+									<li><a href="#">- </a></li>
+									<li><a href="#">Contact</a></li>
+									</ul>
+							</div>
+						</div>
+						<div class="col-lg-4  col-md-6">
+							<div class="single-footer-widget mail-chimp">
+								<h2 class="mb-20"><font color="white" >Contact Us</font></h2>
+								<p>
+									Shri Krushman Co. Op. Housing Society,Block No.05
+									Babariya Main Road,Near By Shri Ashapura Electronics 
+									Rajkot-360002
+								</p>
+								<h3>+91 6353624789</h3>
+								</div>
+						</div> -->							
+						<!-- <div class="col-lg-6  col-md-12"> -->
+				
+
+					<div class="row footer-bottom d-flex justify-content-center">
+					<p class="col-lg-4 col-sm-4 footer-text m-0">
+					</p>
+						<p class="col-lg-8 col-sm-8 footer-text " style="padding-bottom:0px;">
+
+					   <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						This Site is Developed By  <b>Drashti Menpara</b>  And <b>Keyur Somaiya</b> in <script>document.write(new Date().getFullYear());</script> </a></p>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						
+					</div>
+				</div>
+			</footer>
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>			
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="js/easing.min.js"></script>			
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>	
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>	
+ 			<script src="js/jquery-ui.js"></script>			
+			<script src="js/owl.carousel.min.js"></script>						
+			<script src="js/jquery.nice-select.min.js"></script>							
+			<script src="js/mail-script.js"></script>	
+			<script src="js/main.js"></script>	
