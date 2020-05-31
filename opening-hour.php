8a6a354a5d7a402bc729c58cd840040933f0e9fb
keyur1625 <keyursomaiya88@gmail.com>
2020-05-31 17:52:45 +0530

    opening-hour file included

---
+++ opening-hour.php
@@ -0,0 +1,61 @@
+
+	
+		<body>	
+			  <!-- #header -->
+			<?php include "header.php" ?>
+			<!-- start banner Area -->
+			<section class="banner-area relative about-banner" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">				
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								Opening Hour				
+							</h1>	
+							<p class="text-white link-nav"><a href="index.php">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="opening-hour.php"> Opening Hour</a></p>
+						</div>	
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->	
+
+			<!-- Start open-hour Area -->
+			<section class="open-hour-area openhour-page">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-12 open-hour-wrap">
+							<h1>Opening Hours</h1>
+							<p>
+							Experience the best dental care at sudant dental clinic
+							</p>
+							<a class="open-btn" href="#"> <span class="circle"></span>We are open Now</a>
+							<div class="date-list d-flex flex-row justify-content-center">
+								<ul class="colm-1">
+									<li>Monday - saturday</li>
+								<li>Sunday</li>
+								</ul>
+								<ul class="colm-2">
+									<li><span>:</span>   09:00am to 01:00pm &nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;04:00pm to 08:30pm</li>
+									<li><span>:</span>   closed</li>
+								</ul>								
+							</div>
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End open-hour Area -->
+
+			<!-- Start service Area 
+
+			<!-- Start home-about Area -->
+			
+			<!-- End home-about Area -->
+
+			<!-- Start testomial Area -->
+			
+			<!-- End testomial Area -->					
+
+			<!-- start footer Area -->		
+			<?php include "footer.php" ?>
+
+		
