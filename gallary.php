a98ffeb6a8f35bae9ec881439f3a1cb7077dae53
keyur1625 <keyursomaiya88@gmail.com>
2020-05-31 17:51:15 +0530

    gallary file included

---
+++ gallary.php
@@ -0,0 +1,120 @@
+
+		<body>	
+			  <!-- #header -->
+			<?php include "header.php" ?>
+			<!-- start banner Area -->
+			<section class="banner-area relative about-banner" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">				
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								Gallary		
+							</h1>	
+							<p class="text-white link-nav"><a href="./index.php">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="gallary.php"> Gallary block</a></p>
+						</div>	
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+
+			
+
+			<!-- Start service Area -->
+		<section class="service-area section-gap">
+				<div class="container">
+											
+					<div class="row"">
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i1.jpg" height="250" alt="Clinic inauguration" width="410" title="Clinic Inauguration"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/G15.jpg" height="250" alt="Dr.Darshana Kor Somaiya"  title="Dr.Darshana Kor Somaiya" width="410"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i3.jpg" height="250" alt="Patient On Clinic" width="410" title="Patient On Clinic-Sudant Dental Clinic"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i2.jpg" height="250" alt=" Dr.Sanjay Somaiya and Dr.Darshana Somaiya" width="410" title="Dr.Sanjay Somaiya and Dr.Darshana Somaiya"></img>			
+							</div>
+						</div>		
+					</div>
+					
+					<!-- 2nd ow image --->
+					<div class="row" style="margin-top:30px;">
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i5.jpg" height="250" alt="Free Dental Camp Image" width="410" title=" Free Dental Camp Image"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i6.jpg" height="250" alt="Free Dental Camp Image " width="410" title="Free Dental Camp Image"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i7.jpg" height="250" alt="Image of Sudant Clinic" width="410" title="Image Of Sudant Dental Clinic"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i8.jpg" height="250" alt="Free Dental Camp For child-Happiness" width="410" title="Free Dental Camp For Child-Happiness"></img>			
+							</div>
+						</div>		
+					</div>
+					<!-- 2nd ro completed images -->
+					<!-- 3rd row images --->
+					<div class="row" style="margin-top:30px">
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/tith.jpg" height="250" alt="POP teeth Image" width="410" title="POP teeth Image"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i11.jpg" height="250"  alt=" Before and After image of teeth-Sudant Clinic" width="410" title="Before and After Image Of title-Sudant Dental Clinic"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/camp1.jpg" height="250" alt=" Free Dental Camp child-Happiness" width="410" title="Free Dental Camp Child-Happiness"></img>			
+							</div>
+						</div>	
+						<div class="col-lg-3 col-md-6">
+							<div class="single-service">	
+								<img src="img/i10.jpg" height="250"  alt="Sudant Clinic" width="410" title="Sudant Dental Clinic"></img>			
+							</div>
+						</div>		
+					</div>
+					<!-- 3rd row images -->
+		
+				</div>	
+			</section>	<!-- End service Area -->
+
+			<!-- Start home-about Area -->
+			
+			<!-- End home-about Area -->
+
+			<!-- Start testomial Area -->
+			
+			<!-- End testomial Area -->	
+																			
+			<!-- start footer Area -->				
+
+			<!-- Start gallary-page Area -->
+			
+			<!-- start footer Area -->		
+			<?php include "footer.php" ?>
+			<!-- End footer Area -->
+
+			
+		</body>
+	</html>
